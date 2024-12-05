04ed149159d9d88ba2a4e9c0a5aef4c618520cac
Anton Shcherbakov <1mailanton@gmail.com>
2024-12-05 15:27:30 +0100

    add back button

--- news/src/main/java/com/petproject/news/ui/NewsItem.kt
+++ news/src/main/java/com/petproject/news/ui/NewsItem.kt
@@ -1,6 +1,7 @@
 package com.petproject.news.ui
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
--- news_detailed/src/main/java/com/internet/connection/news_detailed/ui/NewsDetailedScreen.kt
+++ news_detailed/src/main/java/com/internet/connection/news_detailed/ui/NewsDetailedScreen.kt
@@ -1,21 +1,35 @@
 package com.internet.connection.news_detailed.ui
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -56,14 +70,43 @@ fun LoadedStateScreen(
     Column (
         modifier = modifier.fillMaxSize()
     ) {
-        AsyncImage(
+        Box(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(260.dp),
-            model = state.news.urlToImage,
-            contentScale = ContentScale.Crop,
-            contentDescription = null
-        )
+        ) {
+            AsyncImage(
+                modifier = Modifier
+                    .fillMaxSize(),
+                model = state.news.urlToImage,
+                contentScale = ContentScale.Crop,
+                contentDescription = null
+            )
+
+            Box(
+                modifier = Modifier
+                    .align(Alignment.TopStart)
+                    .padding(12.dp)
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = rememberRipple(
+                            bounded = false, // For ripple beyond the icon bounds
+                            radius = 28.dp,
+                            color = Color.Gray
+                        ),
+                        onClick = {
+
+                        }
+                    )
+            ) {
+                Icon(
+                    modifier = Modifier.padding(8.dp),
+                    imageVector  = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = stringResource(R.string.back),
+                    tint = Color.White
+                )
+            }
+        }
 
         Text(
             modifier = Modifier
