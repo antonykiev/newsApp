6f9d09359e0eff8a34be5ca9e1f53a39c956c5a4
Anton Shcherbakov <1mailanton@gmail.com>
2024-12-13 11:59:27 +0100

    refactored clean code

--- features/news/src/main/java/com/petproject/news/ui/NewsListScreen.kt
+++ features/news/src/main/java/com/petproject/news/ui/NewsListScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CircularProgressIndicator
@@ -23,6 +24,8 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -74,7 +77,6 @@ fun NewsListScreen(
     }
 }
 
-@OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
 fun ListContent(
     onNewsClick: (articleId: Long) -> Unit,
@@ -97,7 +99,6 @@ fun ListContent(
     }
 }
 
-@OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
 fun LoadedStateScreen(
     state: ListState.Loaded,
