951e803146c7a964c2ba091e7286fd49e8ae135c
Anton Shcherbakov <1mailanton@gmail.com>
2024-12-14 17:42:19 +0100

    customize search bar

--- features/news/src/main/java/com/petproject/news/ui/NewsListScreen.kt
+++ features/news/src/main/java/com/petproject/news/ui/NewsListScreen.kt
@@ -18,12 +18,16 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarColors
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -68,6 +72,9 @@ fun NewsListScreen(
                         contentDescription = null
                     )
                 },
+                colors = SearchBarDefaults.colors(
+                    dividerColor = Color.Transparent
+                ),
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(16.dp)
